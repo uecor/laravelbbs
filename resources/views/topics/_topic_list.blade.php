df2dc533e5df7af362a7ce86ea22e0f85e9e81d5
root <1065792154@qq.com>
2018-06-09 23:36:01 +0800

    Categrory topic list

--- resources/views/topics/_topic_list.blade.php
+++ resources/views/topics/_topic_list.blade.php
@@ -20,12 +20,12 @@
                         </a>
                     </div>
 
-                    <div class="media-body meta">
+                        <div class="media-body meta">
 
-                        <a href="#" title="{{ $topic->category->name }}">
-                            <span class="glyphicon glyphicon-folder-open" aria-hidden="true"></span>
-                             {{ $topic->category->name }}
-                        </a>
+                            <a href="{{ route('categories.show', $topic->category->id) }}" title="{{ $topic->category->name }}">
+                                <span class="glyphicon glyphicon-folder-open" aria-hidden="true"></span>
+                                 {{ $topic->category->name }}
+                            </a>
 
                         <span> • </span>
                         <a href="{{ route('users.show', [$topic->user_id]) }}" title="{{ $topic->user->name }}">
--- routes/web.php
+++ routes/web.php
@@ -35,3 +35,4 @@ Route::resource('users','UsersController',['only'=>['show','update','edit']]);
 
 Route::resource('projects', 'ProjectsController', ['only' => ['index', 'show', 'create', 'store', 'update', 'edit', 'destroy']]);
 Route::resource('topics', 'TopicsController', ['only' => ['index', 'show', 'create', 'store', 'update', 'edit', 'destroy']]);
+Route::resource('categories', 'CategoriesController', ['only' => ['show']]);
