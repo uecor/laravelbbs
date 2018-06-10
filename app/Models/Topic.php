e7d9d26b9ff736b3dd9c56a355bbdc95093afce2
root <1065792154@qq.com>
2018-06-10 08:47:43 +0800

    User can create topic

--- app/Models/Topic.php
+++ app/Models/Topic.php
@@ -7,12 +7,13 @@ class Topic extends Model
     protected $fillable = [
         'title',
         'body',
-        'user_id',
+//        'user_id',
         'category_id',
-        'reply_count',
-        'view_count',
-        'last_reply_user_id',
-        'order', 'excerpt',
+//        'reply_count',
+//        'view_count',
+//        'last_reply_user_id',
+//        'order',
+        'excerpt',
         'slug'
       ];
     public function category()
