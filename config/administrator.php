f1d1ef7ced41e83ba1fe6db5d982a891b2dbd680
root <1065792154@qq.com>
2018-06-20 06:36:48 +0800

    Admin can manage site settings

--- config/administrator.php
+++ config/administrator.php
@@ -36,12 +36,21 @@ return array(
      *      'Analytics' => ['E-Commerce' => 'page.pages.analytics'],
      *  ]
      */
-    'menu' => [
-        '用户与权限' => [
-            'users',
-        ],
-    ],
-
+     'menu' => [
+         '用户与权限' => [
+             'users',
+             'roles',
+             'permissions',
+         ],
+         '内容管理' => [
+             'categories',
+             'topics',
+             'replies',
+         ],
+         '站点管理' => [
+             'settings.site',
+         ],
+     ],
     /*
      * 权限控制的回调函数。
      *
